dce9508203fef0603b62dc6b14493bd7922dea10
ammar <ammarhamed64@gmail.com>
2023-06-15 16:36:28 +0300

    add script for haplo

--- haplo.R
+++ haplo.R
@@ -2,7 +2,7 @@
 install.packages(c("adegenet", "poppr", "dplyr", "hierfstat", "reshape2", "ggplot2", "RColorBrewer", "scales", "kableExtra", "data.table", "magic", "flextable", "pegas"))
 install.packages("haplo.stats")
 install.packages("trio")
-install.packages(arsenal)
+install.packages("arsenal")
 install.packages("readxl")
 # Load the haplo.stats package
 library(haplo.stats)
@@ -12,18 +12,11 @@ library(readxl)
 ls(name="package:haplo.stats")
 help(haplo.em)
 
-
-
-
-
-
-
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install("trio")
 
-# Read in the genetic data as a haplo.stats "geno" object
-geno <- read.pedfile("D:/grad/Haploviewd/SNP_Dokki.ped", format="ped")
+
 
 geno2<- read.csv("D:/grad/Haploviewd/GH_Dokki-Mandara.xlsx")
 lobster = read.csv("D:/grad/popgene/Lobster_SNP_Genotypes.csv")
@@ -35,6 +28,11 @@ geno <- read.pedfile(file = "D:/grad/Haploviewd/SNP_Dokki.txt")
 
 # Read in the genetic data as a haplo.stats "geno" object
 geno <- read_excel("D:/grad/Haploview/GH_Dokki-Mandara.xlsx")
+geno2 <- read_xls("D:/grad/Haploview/Haploview-input_N_Ansari-Pour.xls")
+typeof(geno2)
+
+haplo_data <- haplo.em(geno2)
+
 # Perform haplotype analysis using the EM algorithm
 hap_em <- haplo.em(geno)
 # Print the haplotype frequencies
